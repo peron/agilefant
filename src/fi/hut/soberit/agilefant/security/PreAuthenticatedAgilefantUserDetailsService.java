2d8a42ee84873044d838f7dbde56f4fd3641d0c2
martin <martin.huber@canoo.com>
2012-11-01 16:42:12 +0100

    create user if it does not exist

--- src/fi/hut/soberit/agilefant/security/PreAuthenticatedAgilefantUserDetailsService.java
+++ src/fi/hut/soberit/agilefant/security/PreAuthenticatedAgilefantUserDetailsService.java
@@ -1,17 +1,12 @@
 package fi.hut.soberit.agilefant.security;
 
 import fi.hut.soberit.agilefant.business.UserBusiness;
+import fi.hut.soberit.agilefant.model.User;
 import org.springframework.dao.DataAccessException;
-import org.springframework.security.Authentication;
-import org.springframework.security.AuthenticationException;
-import org.springframework.security.GrantedAuthoritiesContainer;
-import org.springframework.security.GrantedAuthority;
-import org.springframework.security.providers.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.security.*;
 import org.springframework.security.userdetails.AuthenticationUserDetailsService;
-import org.springframework.security.userdetails.User;
 import org.springframework.security.userdetails.UserDetails;
 import org.springframework.security.userdetails.UsernameNotFoundException;
-import org.springframework.util.Assert;
 
 
 public class PreAuthenticatedAgilefantUserDetailsService implements AuthenticationUserDetailsService {
@@ -23,20 +18,20 @@ public class PreAuthenticatedAgilefantUserDetailsService implements Authenticati
         this.userBusiness = userBusiness;
     }
 
-    /**
-     * API method to provide UserDetails-object for given username. Returns
-     * AgilefantUserDetails - instances.
-     */
-    public UserDetails loadUserByUsername(String userName)
-            throws UsernameNotFoundException, DataAccessException {
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException, DataAccessException {
 
 
         // try getting user by given username
         fi.hut.soberit.agilefant.model.User user = userBusiness.retrieveByLoginName(userName);
 
-        // no user found, throw exception
-        if (user == null)
-            throw new UsernameNotFoundException("no such user: " + userName);
+        // no user found, create one, because it IS authenticated
+        if (user == null) {
+            user = new User();
+            user.setLoginName(userName);
+            user.setFullName(userName);
+            user.setAdmin(false);
+            user = userBusiness.storeUser(user, null, "","");
+        }
 
         // success, return UserDetails-instance
         return new AgilefantUserDetails(user);
@@ -44,21 +39,9 @@ public class PreAuthenticatedAgilefantUserDetailsService implements Authenticati
 
     /**
      * Get a UserDetails object based on the user name contained in the given
-     * token, and the GrantedAuthorities as returned by the
-     * GrantedAuthoritiesContainer implementation as returned by
-     * the token.getDetails() method.
+     * token
      */
     public final UserDetails loadUserDetails(Authentication token) throws AuthenticationException {
             return loadUserByUsername(token.getPrincipal().toString());
-    }
-
-    /**
-     * Creates the final <tt>UserDetails</tt> object. Can be overridden to customize the contents.
-     *
-     * @param token       the authentication request token
-     * @param authorities the pre-authenticated authorities.
-     */
-    protected UserDetails createuserDetails(Authentication token, GrantedAuthority[] authorities) {
-        return new User(token.getName(), "N/A", true, true, true, true, authorities);
     }
 }
